d0d97e554e3db14373480d1af2120af3d5fcd005
pavinduLakshan <174084F@uom.lk>
2019-10-31 13:12:57 +0530

    added prod backend url on axios instance

--- src/utils/SENDER.js
+++ src/utils/SENDER.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-        baseURL: 'http://localhost:8080/api',
+        baseURL: process.env.NODE_ENV === "production" ? " https://ontask-back.herokuapp.com/api" :'http://localhost:8080/api',
         headers: {
           'Authorization': 'Bearer '+ localStorage.getItem('token')
         }
